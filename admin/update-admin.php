4337e3f89261a8d58d4e36da5596dddabe37f5e6
PularaW <84730094+PularaW@users.noreply.github.com>
2023-02-03 19:32:46 +0530

    bug fix

--- admin/update-admin.php
+++ admin/update-admin.php
@@ -49,7 +49,6 @@
                 <tr>
                     <td colspan="2">
                     <br />
-                        <!-- <input type="hidden" name="id" value="<?php echo $id ?>"> -->
                         <input type="hidden" name="id" value="<?php  echo htmlspecialchars($id,  ENT_QUOTES, 'UTF-8'); ?>">
                         <input type="submit" name="submit" value="Update Admin" class="btn-update"> 
 
--- admin/update-materials.php
+++ admin/update-materials.php
@@ -129,7 +129,7 @@
 </tr>
 <tr>
          <td>
-             <!-- <input type="hidden" name="id" value="<?php echo $id; ?>"> -->
+             
              <input type="hidden" name="id" value="<?php  echo htmlspecialchars($id,  ENT_QUOTES, 'UTF-8'); ?>">
              <input type="hidden" name="current_image" value="<?php echo $current_image; ?>">
 
